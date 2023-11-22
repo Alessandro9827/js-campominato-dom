fb94731ca7196081861a90aa953186bc733fff4e
Alessandro9827 <ale.moramarco27@gmail.com>
2023-11-22 17:07:27 +0100

    created the selectable and functioning levels

--- js/script.js
+++ js/script.js
@@ -1,31 +1,18 @@
 const mainContentEl = document.querySelector('main section.main-content');
 const playButton = document.querySelector('button#btn');
-let numberArrey = createArreyOfNumber (1, 100);
+const difficultySelectorEl = document.querySelector('select#select-difficulty');
+let numberArray = createArrayOfNumber (1, 100);
 
 playButton.addEventListener('click', function() {
-    for (let i = 1 ; i <= 100 ; i++){
-        const currentSquare = getNewSquare();
-    
-        let randomIndex = getRandomNumber (0, numberArrey.length -1);
-        let randomNumber = numberArrey [randomIndex];
-        numberArrey.slice(randomIndex, 1);
-        currentSquare.innerHTML += `<span> ${randomIndex} </span>`;
-        
-        currentSquare.addEventListener('click', function(){
-        currentSquare.classList.toggle('clicked');
-        currentSquare.classList.add('bg-blue');
-        console.log('hai selezionato la casella n.', randomIndex);
-        });
-        
-        mainContentEl.appendChild(currentSquare);
-    }
+    //nome Funzione che genera la partita
+    generateNewGame(mainContentEl, difficultySelectorEl);
 });
 
 
 //------ Functions ------
 
 /**
- * Creazione quadrato
+ * Creazione quadrati
  * @returns
  */ 
 function getNewSquare() {
@@ -50,10 +37,57 @@ function getRandomNumber(minNumber, maxNumber) {
  * @param  end 
  * @returns 
  */
-function createArreyOfNumber (start, end) {
-    let myArrey = [];
+function createArrayOfNumber (start, end) {
+    let myArray = [];
     for (i = start ; i <= end ; i++) {
-        myArrey.push(i);
+        myArray.push(i);
     }
-    return myArrey;
+    return myArray;
+}
+
+
+function generateNewGame(wrapperElement, levelSelector) {
+    wrapperElement.innerHTML = '';
+
+    const level = parseInt(levelSelector.value);
+    let cellsNo;
+
+    switch (level) {
+        case 0:
+        default:
+            cellsNo = 100;
+            break;
+        case 1:
+            cellsNo = 81;
+            break;
+        case 2:
+            cellsNo = 49;
+            break;
+    }
+
+    let cellsPerRow = Math.sqrt(cellsNo);
+
+
+    for (let i = 1 ; i <= cellsNo ; i++) {
+        const currentSquare = getNewSquare();
+    
+        //Creazione numeri random nella caselle
+        let randomIndex = getRandomNumber (0, numberArray.length -1);
+        let randomNumber = numberArray [randomIndex];
+        numberArray.slice(1, randomIndex);
+        currentSquare.innerHTML += `<span> ${randomIndex} </span>`; //Stampa il numero cas. nella casella
+        
+        const cellSize = `calc(100% / ${cellsPerRow})`;
+        currentSquare.style.width = cellSize;
+        currentSquare.style.height= cellSize;
+
+        //al click dell'utente
+        currentSquare.addEventListener('click', function(){
+        currentSquare.classList.toggle('clicked');
+        currentSquare.classList.add('bg-blue');
+        console.log('hai selezionato la casella n.', randomIndex);
+        });
+        
+        mainContentEl.appendChild(currentSquare);
+    };
 }
